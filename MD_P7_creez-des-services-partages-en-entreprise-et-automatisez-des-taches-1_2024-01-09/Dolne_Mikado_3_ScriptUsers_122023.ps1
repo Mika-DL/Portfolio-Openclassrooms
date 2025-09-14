ad4690bc3ac1af849a442dd3074c0226842d2902
Mikado.D <mikadolne@gmail.com>
2025-09-14 09:07:58 +0200

    Add files via upload

---
+++ MD_P7_creez-des-services-partages-en-entreprise-et-automatisez-des-taches-1_2024-01-09/Dolne_Mikado_3_ScriptUsers_122023.ps1
@@ -0,0 +1,71 @@
+﻿#Création d'utilisateurs dans l'AD (DOLNE Mikado 03/12/2023 ver1.3)
+
+#Importer les données
+$CSVFile = "C:\scripts\AD_USERS\Utilisateurs.csv.txt"
+$CSVData = Import-Csv -Path $CSVFile -Delimiter ";" -Encoding Default
+
+# Boucle Foreach pour parcourir le fichier CSV
+Foreach($Utilisateur in $CSVData){
+
+ $UtilisateurPrenom = $Utilisateur.Prenom       #Récupère le prenom de l'utilisateur dans le fichier CSV
+ $UtilisateurNom = $Utilisateur.Nom             #Récupère le nom de l'utilisateur dans le fichier CSV
+ $UtilisateurLogin = ($UtilisateurPrenom).Substring(0,1).ToLower() + "." + $UtilisateurNom.ToLower()        #On prend la première lettre du prénom et on recupère le nom de famille le tout en minuscules de l'utilisateur
+ $UtilisateurEmail = "$UtilisateurLogin@Axeplane.fr"    #On crée une adresse email
+ $UtilisateurMotdePasse = "Test1234"            
+ $UtilisateurFonction = $Utilisateur.Fonction   #Récupère la fonction de l'utilisateur dans le fichier CSV
+ $UtilisateurService = $Utilisateur.Service     #Récupère le service de l'utilisateur dans le fichier CSV
+ $UtilisateurGroupe = $Utilisateur.Groupe -split ','      #Récupère le groupe de l'utilisateur dans le fichier CSV
+ $UtilisateurOU = $Utilisateur.OU               #Récupère la localisation de l'OU de l'utilisateur
+ $Nom = "$UtilisateurNom $UtilisateurPrenom"
+ $DossierCacher = $UtilisateurLogin + "$"
+
+
+
+  
+   try {
+   # Créer l'utilisateur dans L'AD
+        New-ADUser  -Name "$UtilisateurNom $UtilisateurPrenom" `
+                    -DisplayName "$UtilisateurNom $UtilisateurPrenom" `
+                    -GivenName $UtilisateurPrenom `
+                    -Surname $UtilisateurNom `
+                    -SamAccountName $UtilisateurLogin `
+                    -UserPrincipalName "$UtilisateurLogin@Axeplane.loc" `
+                    -EmailAddress $UtilisateurEmail `
+                    -Title $UtilisateurFonction `
+                    -Department $UtilisateurService `
+                    -Path $UtilisateurOU `
+                    -AccountPassword(ConvertTo-SecureString $UtilisateurMotdePasse -AsPlainText -Force) `
+                    -ChangePasswordAtLogon $true `
+                    -Enabled $true `
+                    -HomeDrive "P:" `
+                    -HomeDirectory "\\SRV-AD\Partages personnels utilisateurs\$DossierCacher"
+
+        Write-Output "Création de l'utilisateur : $UtilisateurLogin ($UtilisateurNom $UtilisateurPrenom)"               
+    
+    # Ajouter l'utilisateur aux groupes spécifiés
+     foreach ($Groupe in $UtilisateurGroupe) {
+        Add-ADGroupMember -Identity $Groupe -Members $UtilisateurLogin
+        Write-Host "L'utilisateur $Nom a été ajouté au groupe $groupe"
+    }
+
+    # Créer et partager le dossier personnel      
+        New-Item -Path "\\SRV-AD\Partages personnels utilisateurs\$DossierCacher" -ItemType Directory
+         # Définir les autorisations sur le dossier   
+            $acl = Get-Acl "\\SRV-AD\Partages personnels utilisateurs\$DossierCacher"
+            $permission = New-Object System.Security.AccessControl.FileSystemAccessRule("$UtilisateurLogin", "FullControl", "Allow")
+            $acl.SetAccessRule($permission)
+        Set-Acl "\\SRV-AD\Partages personnels utilisateurs\$DossierCacher" $acl
+        Write-Output "Création du dossier personnel de l'utilisateur : $DossierCacher ($UtilisateurNom $UtilisateurPrenom)"
+    
+    # Mapper le lecteur réseau
+        #New-PSDrive -Name "P" -PSProvider FileSystem -Root "\\SRV-AD\Partages personnels utilisateurs\$DossierCacher" -Persist
+        #Write-Host "Le Lecteur $DossierCacher a été mappé avec succès"
+    
+    
+    } catch {
+        Write-Host "Une erreur est apparue : $_"
+    }
+
+    }
+    
+ 
---
+++ MD_P7_creez-des-services-partages-en-entreprise-et-automatisez-des-taches-1_2024-01-09/Dolne_Mikado_4_ScriptMDP_122023.ps1
@@ -0,0 +1,38 @@
+﻿#Demande de réinitialisation de mot de passe d'un utilisateur de l'AD (DOLNE Mikado v1.0)
+
+
+# Définir le chemin du fichier de log
+$LogFile = "C:\scripts\Réinitialisation mot de passe\log\ResetPasswordLog.txt"
+
+# Fonction pour ajouter une entrée dans le fichier de log
+function Add-LogEntry {
+    param (
+        [string]$Message
+    )
+    $LogEntry = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $Message"
+    $LogEntry | Out-File -Append -FilePath $LogFile
+}
+
+# Demander le nom d'utilisateur à réinitialiser
+$Utilisateur = Read-Host "Entrez le nom d'utilisateur à réinitialiser"
+
+# Vérifier si l'utilisateur existe
+if (Get-ADUser -Filter {SamAccountName -eq $Utilisateur -or Name -eq $Utilisateur}) {
+    # Demander le motif de réinitialisation du mot de passe
+    $Motif = Read-Host "Motif de réinitialisation du mot de passe (ex: incident, oubli, demande de l'utilisateur,etc...)"
+
+    try {
+        # Réinitialiser le mot de passe avec changement à la prochaine connexion
+        Set-ADAccountPassword -Identity $Utilisateur -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "NewPassword123" -Force)
+        Set-ADUser -Identity $Utilisateur -ChangePasswordAtLogon $true
+
+        # Enregistrer l'entrée dans le fichier de log
+        Add-LogEntry -Message "Réinitialisation du mot de passe pour l'utilisateur $Utilisateur Motif : $Motif"
+
+        Write-Host "Mot de passe réinitialisé avec succès pour l'utilisateur $Utilisateur. Motif : $Motif"
+    } catch {
+        Write-Host "Erreur lors de la réinitialisation du mot de passe : $_"
+    }
+} else {
+    Write-Warning "L'utilisateur $Utilisateur n'existe pas dans l'AD."
+}
